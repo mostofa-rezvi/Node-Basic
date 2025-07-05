2c89d2540290c75f494b7d45e2b41dd384ecdd0e
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2025-07-05 22:36:18 +0600

    Login API

--- job-portal/controllers/authController.js
+++ job-portal/controllers/authController.js
@@ -38,3 +38,37 @@ export const registerController = async (req, res, next) => {
     token,
   });
 };
+
+export const loginController = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //validation
+  if (!email || !password) {
+    return next("Please provide all fields.");
+  }
+
+  // find user by email
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    return next("Invalid Username or Password");
+  }
+
+  // compare password
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    return next("Invalid Username or Password");
+  }
+
+  const token = user.createJWT();
+
+  res.status(200).json({
+    success: true,
+    message: "Login Successful",
+    user: {
+      name: user.name,
+      email: user.email,
+      location: user.location,
+    },
+    token,
+  });
+};
--- job-portal/routes/authRoutes.js
+++ job-portal/routes/authRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import {
+  loginController,
+  registerController,
+} from "../controllers/authController.js";
 
 // router object
 const router = express.Router();
 
 // routes
+// register || POST
 router.post("/register", registerController);
+
+// login || POST
+router.post("/login", loginController);
 
 // export
 export default router;
