d3d136c0b5aa786d73af89dd328980e991670435
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2025-07-07 11:32:51 +0600

    bulk data insert - jobs data

--- jobportal/routes/jobRoutes.js
+++ jobportal/routes/jobRoutes.js
@@ -6,6 +6,7 @@ import {
   getAllJobController,
   updateJobController,
 } from "../controller/jobController.js";
+// import jobModel from "../models/jobModel.js";
 
 const router = express.Router();
 
@@ -21,5 +22,28 @@ router.patch("/update-job/:id", userAuth, updateJobController);
 
 // Delete Job || DELETE
 router.delete("/delete-job/:id", userAuth, deleteJobController);
+
+// Bulk Insert Jobs || POST
+// router.post("/bulk-insert", userAuth, async (req, res, next) => {
+//   try {
+//     const jobs = req.body;
+
+//     if (!Array.isArray(jobs) || jobs.length === 0) {
+//       return res.status(400).json({ message: "No job data provided." });
+//     }
+
+//     const jobsWithUser = jobs.map((job) => ({
+//       ...job,
+//       createdBy: req.user.userId,
+//     }));
+
+//     const insertedJobs = await jobModel.insertMany(jobsWithUser);
+//     res.status(201).json({
+//       message: `${insertedJobs.length} jobs inserted successfully.`,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 export default router;
